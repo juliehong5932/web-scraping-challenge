2e913892ba197451be76594eb2550adb7388cb85
juliehong5932 <juliehong5932@gmail.com>
2020-09-20 21:47:28 -0600

    final commit

--- Missions_to_Mars/flask_app.py
+++ Missions_to_Mars/flask_app.py
@@ -1,25 +1,22 @@
-from flask import Flask, render_template
-import pymongo
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import scrape_mars
 
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+mongo=PyMongo(app)
 
-# connect to mongo db and collection
-db = client.mars
-produce = db.produce
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    # write a statement that finds all the items in the db and sets it to a variable
-    inventory = list(produce.find())
-    print(inventory)
-
-    # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", inventory=inventory)
-
+    results = mongo.db.mars.find_one()
+    return render_template("index.html", mars_data=results)
+    
+@app.route('/scrape')
+def scrape():
+    data=scrape_mars.scrape_all()
+    mongo.db.mars.update({}, mars_data, upsert=True)
+    return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup as bs
 from splinter import Browser
 import requests
 import pandas as pd
-import pymongo
+from flask_pymongo import PyMongo
 
 def init_browser():
     executable_path = {'executable_path': 'chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
-
+    
 def scrape():
     browser = init_browser()
     news_url = 'https://mars.nasa.gov/news/'
@@ -19,7 +19,9 @@ def scrape():
     article = soup.find("div", class_='list_text')
     news_title = article.find('div', class_='content_title').find('a').text
     news_p = article.find('div', class_='article_teaser_body').text
+    return (news_title, news_p)
 
+def scrape():
     jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(jpl_url)
     image_html = browser.html
@@ -28,42 +30,36 @@ def scrape():
     images_url = images_soup.find_all('img')[3]["src"]
     base_url = 'https://www.jpl.nasa.gov'
     featured_image_url = base_url + images_url 
-    
+    return featured_image_url
+
+def scrape():
     fact_url = 'https://space-facts.com/mars/'
     fact_df = pd.read_html(fact_url)
     mars_fact_df = fact_df[2]
     mars_fact_df.columns = ["Description", "Data"]
     mars_html_table = mars_fact_df.to_html()
     mars_html_table.replace('\n', '')
+    return mars_html_table
 
+def scrape():
     astrogeology_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(astrogeology_url)
     hemisphere_html = browser.html
     hemisphere_soup = bs(hemisphere_html, 'html.parser')
     
-    hemisphere = hemisphere_soup.find("div", class_='collapsible results')
+    hemisphere = hemisphere_soup.find_all("div", class_='item')
 
-    hemisphere_title = hemisphere.find("div", class_='description').h3.text
-    base_link = 'https://astrogeology.usgs.gov'
-    hemisphere_link = base_link + hemisphere.a['href']
-    
     hemisphere_image_urls = []
 
-    for i in hemisphere:
-        hemisphere_title = hemisphere.find("div", class_='description').h3.text
-    
+    for each_h in hemisphere:
+        hemisphere_title = each_h.find("div", class_='description').h3.text
         base_link = 'https://astrogeology.usgs.gov'
-        hemisphere_link = hemisphere.a['href']
-        browser.visit(base_link + hemisphere_link)
-        
-        hemisphere_image_html = browser.html
-        hemisphere_image_soup = bs(hemisphere_image_html, 'html.parser')
-        
-        image_link = hemisphere_image_soup.find('div', class_='downloads')
-        image_url = image_link.find('li').a['href']
-        
+        hemisphere_link = base_link + each_h.a['href']
+
         image_dict = {}
         image_dict['title'] = hemisphere_title
         image_dict['img_url'] = hemisphere_link
         
         hemisphere_image_urls.append(image_dict)
+    return hemisphere_image_urls
+
